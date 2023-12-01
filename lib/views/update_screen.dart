7a6e4cb49d3f27b972eca779be154e3774efa01b
divyanshu <techistics1@gmail.com>
2023-12-01 11:02:37 +0530

    Basic Fixes

---
+++ lib/views/update_screen.dart
@@ -0,0 +1,171 @@
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../bloc/auth_bloc.dart';
+import '../animation/delayed_animation.dart';
+import '../event/auth_event.dart';
+import '../terms/terms_and_conditions.dart';
+
+class UpdateScreen extends StatefulWidget {
+  @override
+  _UpdateScreenState createState() => _UpdateScreenState();
+}
+
+class _UpdateScreenState extends State<UpdateScreen>
+    with SingleTickerProviderStateMixin {
+  final int delayedAmount = 500;
+  double? _scale;
+  AnimationController? _controller;
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(
+        milliseconds: 200,
+      ),
+      lowerBound: 0.0,
+      upperBound: 0.1,
+    )..addListener(() {
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Colors.white;
+    _scale = 1 - _controller!.value;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          backgroundColor: Colors.red,
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                AvatarGlow(
+                  endRadius: 90,
+                  duration: Duration(seconds: 2),
+                  glowColor: Colors.white24,
+                  repeat: true,
+                  repeatPauseDuration: Duration(seconds: 2),
+                  startDelay: Duration(seconds: 1),
+                  child: Material(
+                      elevation: 8.0,
+                      shape: CircleBorder(),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.grey[100],
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(58),
+                            child: Image.asset("assets/images/logo.png")),
+                        radius: 50.0,
+                      )),
+                ),
+                DelayedAnimation(
+                  child: Text(
+                    "Update Required",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35.0,
+                        color: color),
+                  ),
+                  delay: delayedAmount + 1000,
+                ),
+                DelayedAnimation(
+                  child: Text(
+                    "",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35.0,
+                        color: color),
+                  ),
+                  delay: delayedAmount + 2000,
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                DelayedAnimation(
+                  child: Text(
+                    "An update is required",
+                    style: TextStyle(fontSize: 20.0, color: color),
+                  ),
+                  delay: delayedAmount + 3000,
+                ),
+                DelayedAnimation(
+                  child: Text(
+                    "to continue using app!",
+                    style: TextStyle(fontSize: 20.0, color: color),
+                  ),
+                  delay: delayedAmount + 3000,
+                ),
+                SizedBox(
+                  height: 100.0,
+                ),
+                DelayedAnimation(
+                  child: GestureDetector(
+                    onTapDown: _onTapDown,
+                    onTapUp: _onTapUp,
+                    child: Transform.scale(
+                      scale: _scale,
+                      child: _animatedButtonUI,
+                    ),
+                  ),
+                  delay: delayedAmount + 4000,
+                ),
+                SizedBox(
+                  height: 50.0,
+                ),
+
+              ],
+            ),
+          )
+
+      ),
+    );
+  }
+
+  Widget get _animatedButtonUI => Container(
+    height: 60,
+    width: 270,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(100.0),
+      color: Colors.white,
+    ),
+    child: GestureDetector(
+      onTap: () {
+        _launchUrl("https://play.google.com/store/apps/details?id=com.talkto.stranger");
+
+      },
+      child: Center(
+        child: Text(
+          'Update',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF8185E2),
+          ),
+        ),
+      ),
+    ),
+  );
+
+  void _onTapDown(TapDownDetails details) {
+    _controller!.forward();
+  }
+
+  void _onTapUp(TapUpDetails details) {
+    _controller!.reverse();
+  }
+  @override
+  void dispose() {
+    _controller!.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
+  Future<void> _launchUrl(String _url) async {
+    if (!await launchUrl(Uri.parse(_url))) {
+      throw Exception('Could not launch $_url');
+    }}
+}
